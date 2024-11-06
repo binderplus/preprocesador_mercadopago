88f2e6422869047b093deae6453c06aab211cb0a
Mijail Todorovich <mijailtodorovich+git@gmail.com>
2024-11-06 11:50:59 -0300

    Actualiza ui

--- src/App.jsx
+++ src/App.jsx
@@ -3,16 +3,22 @@ import './App.css'
 import {FileHandler} from "./FileHandler.jsx";
 
 function App() {
-    const [count, setCount] = useState(0)
 
     return (
         <>
-            <h1>
-                Preprocesador de reportes de <br/>
-                conciliación de MercadoPago
+            <h1 className="title">
+                Preprocesador de reportes de conciliación de MercadoPago
             </h1>
 
-            <FileHandler />
+            <p className="read-the-docs">
+                Cargue el reporte de liquidaciones para obtener un reporte con un formato similar al de un extracto
+                bancario tradicional.
+
+                Para un correcto funcionamiento, configure el reporte para que incluya todas las columnas y que los
+                nombres de las columnas estén en inglés. <a href="https://github.com/binderplus/preprocesador_mercadopago/">Más información.</a>
+            </p>
+
+            <FileHandler/>
 
         </>
     )
